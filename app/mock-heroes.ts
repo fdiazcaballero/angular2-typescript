828a6d2da48a5f92e43b66b8aa489299a61b5466
fdiazcaballero <fdiazcaballero@gmail.com>
2016-04-25 16:03:19 +0200

    routing

--- app/mock-heroes.ts
+++ app/mock-heroes.ts
@@ -1,11 +1,11 @@
 import {Hero} from './hero';
 export var HEROES: Hero[] = [
-    {"id": 11, "name": "Mr. Nice"},
-    {"id": 12, "name": "Narco"},
-    {"id": 13, "name": "Bombasto"},
-    {"id": 14, "name": "Celeritas"},
-    {"id": 15, "name": "Magneta"},
-    {"id": 16, "name": "RubberMan"},
+    {"id": 11, "name": "Tihomir"},
+    {"id": 12, "name": "Riccardo"},
+    {"id": 13, "name": "Fabrizio"},
+    {"id": 14, "name": "Tuttologo"},
+    {"id": 15, "name": "DepressionJS"},
+    {"id": 16, "name": "ConfussionJS"},
     {"id": 17, "name": "Dynama"},
     {"id": 18, "name": "Dr IQ"},
     {"id": 19, "name": "Magma"},
